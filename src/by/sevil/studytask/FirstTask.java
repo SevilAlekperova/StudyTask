253aa4a28804a9240637a37db29d861d57a4ff1c
salekperova <s.alekperova1997@mail.ru>
2022-04-28 15:25:28 +0300

    решение 1 задачи

---
+++ src/by/sevil/studytask/FirstTask.java
@@ -0,0 +1,16 @@
+package by.sevil.studytask;
+
+import java.util.Scanner;
+
+public class FirstTask {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите дату рождение:");
+        int number = scanner.nextInt();
+        System.out.println("Поздравляем!Вы родились " + number + " числа");
+
+    }
+}
+
+

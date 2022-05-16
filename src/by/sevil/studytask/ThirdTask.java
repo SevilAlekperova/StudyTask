7e48f3520f3a4f320ad24074887ce5032a4cc788
salekperova <s.alekperova1997@mail.ru>
2022-05-16 18:13:59 +0300

    решение 3 задачи

---
+++ src/by/sevil/studytask/ThirdTask.java
@@ -0,0 +1,11 @@
+package by.sevil.studytask;
+
+import java.util.Scanner;
+
+public class ThirdTask {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}

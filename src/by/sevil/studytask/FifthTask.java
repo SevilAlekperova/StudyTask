3c836ed3a953f2d9faf99ea8c8ac4932fec773e3
salekperova <s.alekperova1997@mail.ru>
2022-05-16 18:29:51 +0300

    решение 5 задачи

---
+++ src/by/sevil/studytask/FifthTask.java
@@ -0,0 +1,9 @@
+package by.sevil.studytask;
+
+public class FifthTask {
+    public static void main(String[] arg) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "*" + 3 + "=" + i * 3);
+        }
+    }
+}
